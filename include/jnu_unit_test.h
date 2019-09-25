b72b3394a24bc69a48b68a96ba9de05611b1eadd
jni <nijinboc@hotmail.com>
2019-09-25 15:26:07 -0400

    Implemented jnu unit test

--- include/jnu_unit_test.h
+++ include/jnu_unit_test.h
@@ -1,32 +1,89 @@
 #ifndef JNU_UNIT_TEST_H
 #define JNU_UNIT_TEST_H
 
+#include <stdio.h>
 #include "jnu_defines.h"
 
-#define JNU_UT_MSG_A(F, ...) printf(F "\n", ##__VA_ARGS__)
-#define JNU_UT_MSG(F) JNU_UT_MSG_A("%s", F)
+#define JNU_UT_MSG_A(D, F, ...) printf("%*s" F "\n", D, "", ##__VA_ARGS__)
+#define JNU_UT_MSG(D, F) JNU_UT_MSG_A(D, "%s", F)
 
-#define JNU_UT_OUTPUT_A(L, F, ...) JNU_UT_MSG_A("%s: In function '%s':\n" \
-                                                "%s:%d: " #L ": " F,\
-                                                __FILE__, \
-                                                __PRETTY_FUNCTION__, \
-                                                __FILE__, \
-                                                __LINE__, \
-                                                ##__VA_ARGS__)
-#define JNU_UT_OUTPUT(L, F) JNU_UT_OUTPUT_A(L, "%s", F)
+#define JNU_UT_OUTPUT_A(D, L, F, ...) JNU_UT_MSG_A(\
+                                      D, "%s:%d: " #L ": " F, \
+                                      __FILE__, \
+                                      __LINE__, \
+                                      ##__VA_ARGS__)
 
-#define JNU_UT_INFO_A(F, ...) JNU_UT_OUTPUT_A(info, F, ##__VA_ARGS__);
-#define JNU_UT_INFO(F) JNU_UT_OUTPUT(info, F);
+#define JNU_UT_OUTPUT(D, L, F) JNU_UT_OUTPUT_A(D, L, "%s", F)
 
-#define JNU_UT_WARN_A(F, ...) JNU_UT_OUTPUT_A(warn, F, ##__VA_ARGS__);
-#define JNU_UT_WARN(F) JNU_UT_OUTPUT(warn, F);
+#define JNU_UT_INFO_A(D, F, ...) JNU_UT_OUTPUT_A(D, info, F, ##__VA_ARGS__)
+#define JNU_UT_INFO(D, F) JNU_UT_OUTPUT(D, info, F)
 
-#define JNU_UT_ERROR_A(F, ...) JNU_UT_OUTPUT_A(error, F, ##__VA_ARGS__);
-#define JNU_UT_ERROR(F) JNU_UT_OUTPUT(error, F);
+#define JNU_UT_WARN_A(D, F, ...) JNU_UT_OUTPUT_A(D, warn, F, ##__VA_ARGS__)
+#define JNU_UT_WARN(D, F) JNU_UT_OUTPUT(D, warn, F)
 
-#define JNU_UT_CHECK(E) if (!(E)) { JNU_UT_ERROR("Check failed: " #E); }
+#define JNU_UT_ERROR_A(D, F, ...) JNU_UT_OUTPUT_A(D, error, F, ##__VA_ARGS__)
+#define JNU_UT_ERROR(D, F) JNU_UT_OUTPUT(D, error, F)
+
+#define JNU_UT_CHECK_A(E, M) if (!jnu::TestCase::Check(E)) {\
+                               JNU_UT_ERROR(jnu::TestCase::Indent(), M); \
+                             }
+
+#define JNU_UT_CHECK(E) JNU_UT_CHECK_A(\
+                        E, "Check failed: " #E " is not true");
+
+#define JNU_UT_EQUAL(A, B) JNU_UT_CHECK_A(\
+                           (A) == (B), \
+                           "Check equal failed: " \
+                           #A " is not equal to " #B)
+
+#define JNU_UT_CLOSE(A, B, P) JNU_UT_CHECK_A(\
+                              (A) - (B) < (P) && (B) - (A) < (P), \
+                              "Check close failed: " \
+                              #A " is not close to " #B)
 
 namespace jnu {
+class TestCase {
+  const static int INDENT = 2;
+  class Report {
+  public:
+    Report();
+    ~Report();
+    Report(const Report& r);
+    Report& operator=(const Report& r);
+    Report& operator+=(const Report& r);
+    bool operator+=(bool passed);
+    Report operator+(const Report& r) const;
+    operator bool() const;
+    unsigned int GetTotal() const { return m_total; }
+    unsigned int GetPassed() const { return m_passed; }
+  private:
+    unsigned int m_total;
+    unsigned int m_passed;
+  };
+public:
+  TestCase();
+  virtual ~TestCase();
+  void Start(const char* name);
+  void Run(const char* name, TestCase& tc);
+  template<typename C, typename... A>
+  void Run(const char* name, A... a) {
+    C c(a...);
+    Run(name, c);
+  }
+  static void Exit(bool passed);
+  void Exit();
+  static void Succ();
+  static void Fail();
+protected:
+  bool Check(bool passed);
+  virtual void Test() {};
+  int Indent() const { return m_indent + INDENT; }
+private:
+  void Reset(int indent);
+  const Report& Run(int indent, const char* name);
+  mutable int m_indent;
+  mutable Report m_report;
+};
 }
 
 #endif
---
+++ jnu/src/jnu_unit_test.cc
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include "jnu_unit_test.h"
+
+using namespace jnu;
+
+inline TestCase::Report::Report()
+  : m_total (0),
+    m_passed (0) {
+}
+inline TestCase::Report::~Report() {
+}
+inline TestCase::Report::Report(const Report& r) {
+  *this = r;
+}
+inline TestCase::Report&
+TestCase::Report::operator=(const Report& r) {
+  m_total = r.m_total;
+  m_passed = r.m_passed;
+  return *this;
+}
+inline TestCase::Report&
+TestCase::Report::operator+=(const Report& r) {
+  m_total += r.m_total;
+  m_passed += r.m_passed;
+  return *this;
+}
+inline bool TestCase::Report::operator+=(bool passed) {
+  ++m_total;
+  if (passed) {
+    ++m_passed;
+  }
+  return passed;
+}
+inline TestCase::Report
+TestCase::Report::operator+(const Report& r) const {
+  Report t = *this;
+  t += r;
+  return t;
+}
+inline TestCase::Report::operator bool() const {
+  return m_total == m_passed;
+}
+TestCase::TestCase()
+  : m_indent (0) {
+}
+TestCase::~TestCase() {
+}
+void TestCase::Start(const char* name) {
+  Run(m_indent, name);
+}
+void TestCase::Run(const char* name, TestCase& tc) {
+  const Report& r = tc.Run(m_indent + INDENT, name);
+  m_report += r;
+}
+void TestCase::Exit(bool passed) {
+  exit(passed ? 0 : 1);
+}
+void TestCase::Exit() {
+  Exit(m_report);
+}
+void TestCase::Succ() {
+  Exit(true);
+}
+void TestCase::Fail() {
+  Exit(false);
+}
+bool TestCase::Check(bool passed) {
+  return m_report += passed;
+}
+inline void TestCase::Reset(int indent) {
+  m_indent = indent;
+  m_report = Report();
+}
+inline const TestCase::Report&
+TestCase::Run(int indent, const char* name) {
+  Reset(indent);
+  JNU_UT_MSG_A(m_indent, "Entering %s", name);
+  Test();
+  JNU_UT_MSG_A(m_indent, "Leaving %s (%u/%u)",
+               name,
+               m_report.GetPassed(),
+               m_report.GetTotal());
+  return m_report;
+}
