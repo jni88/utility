fa2f4cd885db416a4d280231d5b3a5685b421f4c
jni <nijinboc@hotmail.com>
2019-11-22 23:19:34 -0500

    Add case insensitive comparison

--- include/jnu_string.h
+++ include/jnu_string.h
@@ -1,12 +1,13 @@
 #ifndef JNU_STRING_H
 #define JNU_STRING_H
 
-#include <string>
-#include "jnu_array.h"
-
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
+
+#include <ctype.h>
+#include <string>
+#include "jnu_array.h"
 
 namespace jnu {
 class StringView {
@@ -51,24 +52,19 @@ public:
   }
   template<typename T, typename... S>
   int Compare(const T& s, S... sz) const {
-    StringView v(s, sz...);
-    return m_size < v.m_size ? -1 :
-           m_size > v.m_size ? 1 :
-           memcmp(m_data, v.m_data, m_size);
+    return CompareT<StrCmp>(s, sz...);
+  }
+  template<typename T, typename... S>
+  int CompareCase(const T& s, S... sz) const {
+    return CompareT<StrCmpCase>(s, sz...);
   }
   template<typename T, typename... S>
   int CompareA(const T& s, S... sz) const {
-    StringView v(s, sz...);
-    int r = 0;
-    size_t size = m_size;
-    if (m_size < v.m_size) {
-      r = -1;
-    } else if (m_size > v.m_size) {
-      r = 1;
-      size = v.m_size;
-    }
-    int p = memcmp(m_data, v.m_data, size);
-    return p == 0 ? r : p;
+    return CompareAT<StrCmp>(s, sz...);
+  }
+  template<typename T, typename... S>
+  int CompareACase(const T& s, S... sz) const {
+    return CompareAT<StrCmpCase>(s, sz...);
   }
   template<typename T>
   bool operator==(const T& s) const {
@@ -85,6 +81,24 @@ public:
   template<typename T>
   bool operator>(const T& s) const {
     return CompareA(s) > 0;
+  }
+  template<typename T, typename... S>
+  bool IsSame(const T& t, S... sz) const {
+    StringView v(t, sz...);
+    return m_size == v.m_size &&
+           (IsEmpty() || m_data == v.m_data);
+  }
+  template<typename T, typename... S>
+  bool IsSameData(const T& t, S... sz) const {
+    StringView v(t, sz...);
+    return m_data == v.m_data ||
+           (IsEmpty() && v.IsEmpty());
+  }
+  template<typename T, typename... S>
+  const char* SubStr(const T& t, S... sz) const {
+    StringView v(t, sz...);
+    return (const char*) memmem(m_data, m_size,
+                                v.m_data, v.m_size);
   }
   void ExpandBegin(size_t t) {
     m_data -= t;
@@ -246,6 +260,41 @@ public:
     return m_size <= 0;
   }
 private:
+  static int ChrCmp(char a, char b) {
+    return (int) a - (int) b;
+  }
+  static int ChrCmpCase(char a, char b) {
+    return toupper(a) - toupper(b);
+  }
+  static int StrCmp(const char* a, const char* b, size_t sz) {
+    return memcmp(a, b, sz);
+  }
+  static int StrCmpCase(const char* a, const char* b, size_t sz) {
+    return strncasecmp(a, b, sz);
+  }
+  template<int (*F)(const char*, const char*, size_t),
+           typename T, typename... S>
+  int CompareT(const T& s, S... sz) const {
+    StringView v(s, sz...);
+    return m_size < v.m_size ? -1 :
+           m_size > v.m_size ? 1 :
+           (*F)(m_data, v.m_data, m_size);
+  }
+  template<int (*F)(const char*, const char*, size_t),
+           typename T, typename... S>
+  int CompareAT(const T& s, S... sz) const {
+    StringView v(s, sz...);
+    int r = 0;
+    size_t size = m_size;
+    if (m_size < v.m_size) {
+      r = -1;
+    } else if (m_size > v.m_size) {
+      r = 1;
+      size = v.m_size;
+    }
+    int p = (*F)(m_data, v.m_data, size);
+    return p == 0 ? r : p;
+  }
   const char* m_data;
   size_t m_size;
 };
